2a07fbdade95451b0bd8e44a021656fb983d803b
eralpkor <eralp.ny@gmail.com>
2020-05-11 19:19:31 -0400

    Add stack_queue_doubly directory and files

---
+++ stack_queue_doubly/stack.py
@@ -0,0 +1,38 @@
+# import sys
+# sys.path.append("../doubly_linked_list")
+import doubly_linked_list
+# from doubly_linked_list.doubly_linked_list import LinkedList
+"""
+A stack is a data structure whose primary purpose is to store and
+return elements in Last In First Out order. 
+
+1. Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+2. Re-implement the Stack class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Stack tests pass.
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Stack?
+"""
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.append(value)
+        self.size += 1
+        # self.size = len(self.storage)
+
+    def pop(self):
+        if self.size > 0:
+            result = self.storage.pop()
+            # self.size = len(self.storage)
+            self.size -= 1
+            return result
+
+        
+

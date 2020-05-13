f8afd55b7c3b8c0c1ecd0c43c97c0c912d77cf13
eralpkor <eralp.ny@gmail.com>
2020-05-13 16:17:36 -0400

    Finish insert, contains and in_order_print :python:

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,9 +5,9 @@ at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
-   on the BSTNode class.
+   on the BinarySearchTree class.
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
-   on the BSTNode class.
+   on the BinarySearchTree class.
 """
 class BinarySearchTree:
     def __init__(self, value):
@@ -23,21 +23,29 @@ class BinarySearchTree:
             if  not self.left:
                 self.left = BinarySearchTree(value)
             else:
-                self.left._insert(value)
+                self.left.insert(value)
         else:
             if not self.right:
                 self.right = BinarySearchTree(value)
             else:
-                self.right._insert(value)
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # when we start searching, self will be the root
+        # compare the target against self
+        # Criteria for returning False: we know we need to go in one direction
+        # but there's nothing in the left or right direction 
+        # check if value is target (if it is return true)
         if target == self.value:
             return True
+        # check if target is greater then the Node value
         elif target > self.value:
+            # go right if right is a BSTNode
             return self.right.contains(target) if self.right else False
         else:
+            # go left if left is a BST
             return self.left.contains(target) if self.left else False
         
 
@@ -54,9 +62,9 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        node.left and self._in_order_print(node.left)
+        node.left and self.in_order_print(node.left)
         print(node.value)
-        node.right and self._in_order_print(node.right)
+        node.right and self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -78,3 +86,19 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+
+bst = BinarySearchTree(20)
+bst.left = BinarySearchTree(10)
+bst.right = BinarySearchTree(34)
+bst.left.left = BinarySearchTree(4)
+bst.left.right = BinarySearchTree(50)
+bst.right.left = BinarySearchTree(21)
+
+print(bst.contains(20))
+print(bst.contains(10))
+print(bst.contains(34))
+
+print(bst.contains(50))
+print(bst.contains(21))
